606f28b645efda3399506e6fbad04bb052084c97
herdifariz <fariz.herdi21@gmail.com>
2024-06-09 19:37:52 +0700

    update data penyakit kepanjangan

--- model/DiseasesData.js
+++ model/DiseasesData.js
@@ -4,55 +4,74 @@ const diseases = [
     description:
       "White tail disease (WTD) or white muscle disease (WMD) is a viral infection caused by Macrobrachium rosenbergii nodavirus (MrNV) and its associated extra small virus (XSV).",
     treatment: "Melafix,Pimafix,Formalin",
-    image: "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2021/12/06/2319888566.jpg",
+    image:
+      "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2021/12/06/2319888566.jpg",
   },
   {
     name: "Parasitic",
-    description: "Fish parasitic diseases result from infections, both internal and external, causing organ damage and system disruptions, crucially impacting aquaculture population health.",
-    treatment: "Organophosphates,Formalin,Potassium Permanganate,2% Salt Bath, Malachite Green Oxalate",
-    image: "http://www.wetwebmedia.com/Arthropoda/CrustaceanPIX/Isopod%20PIX/Anilocra%20laticaudataMD%20BONside.jpg",
+    description:
+      "Fish parasitic diseases result from infections, both internal and external, causing organ damage and system disruptions, crucially impacting aquaculture population health.",
+    treatment:
+      "Organophosphates,Formalin,Potassium Permanganate,2% Salt Bath, Malachite Green Oxalate",
+    image:
+      "http://www.wetwebmedia.com/Arthropoda/CrustaceanPIX/Isopod%20PIX/Anilocra%20laticaudataMD%20BONside.jpg",
   },
   {
     name: "Bacterial Red/Red Spot",
-    description: "Red spot disease in fish, caused by Pseudomonas anguilliseptica, leads to high mortality rates due to its rapid spread and detrimental effects on aquaculture.",
+    description:
+      "Red spot disease in fish, caused by Pseudomonas anguilliseptica, leads to high mortality rates due to its rapid spread and detrimental effects on aquaculture.",
     treatment: "Praziquantel (Antibiotic) ",
-    image: "https://www.dpi.nsw.gov.au/__data/assets/image/0008/1268360/EUS-Bony-Bream-570x370.jpg",
+    image:
+      "https://www.dpi.nsw.gov.au/__data/assets/image/0008/1268360/EUS-Bony-Bream-570x370.jpg",
   },
   {
     name: "Bacterial Aeromoniasis",
-    description: "Bacterial aeromoniasis is a disease caused by the bacterium Aeromonas hydrophila in fish.",
+    description:
+      "Bacterial aeromoniasis is a disease caused by the bacterium Aeromonas hydrophila in fish.",
     treatment: "Pimafix,Furan-2,Triple Sulfa,E.M Erythromycin",
-    image: "https://www.shutterstock.com/image-photo/fish-infectionskin-ulcerativeaeromonas-hydrophila-infect-600nw-1998990323.jpg",
+    image:
+      "https://www.shutterstock.com/image-photo/fish-infectionskin-ulcerativeaeromonas-hydrophila-infect-600nw-1998990323.jpg",
   },
   {
     name: "Fungal Saprolegniasis",
-    description: "Saprolegniasis, caused by Saprolegnia spp., damages fish tissue and weakens immunity. Control involves using chitinolytic microorganisms like Bacillus licheniformis and Streptomyces olivaceoviridis.",
-    treatment: "Primafix,Formaldehyde(37%),Malachite Green Oxalate,Methylene Blue,Salt,Providone-iodine,Potassium Permanganate",
-    image: "https://aquariumscience.org/wp-content/uploads/2020/08/fungus-9.jpg",
+    description:
+      "Saprolegniasis, caused by Saprolegnia spp., damages fish tissue and weakens immunity. Control involves using chitinolytic microorganisms like Bacillus licheniformis and Streptomyces olivaceoviridis.",
+    treatment:
+      "Primafix,Formaldehyde(37%),Malachite Green Oxalate,Methylene Blue,Salt,Providone-iodine,Potassium Permanganate",
+    image:
+      "https://aquariumscience.org/wp-content/uploads/2020/08/fungus-9.jpg",
   },
   {
     name: "Bacterial Gill",
-    description: "Bacterial gill disease (BGD), identified in the 1920s, affects hatchery trout, causing gill filament clubbing due to bacteria. Environmental factors influence severity, impacting fish health by hindering respiration and balance. Similar issues can stem from diseases, deficiencies, toxins, or pollutants.",
+    description:
+      "Bacterial gill disease (BGD) affects hatchery trout, causing gill filament clubbing. Environmental factors influence severity, impacting fish health by hindering respiration and balance. Similar issues can stem from diseases, toxins, or pollutants.",
     treatment: "Melafix,Formalin,Praziquantel",
-    image: "https://www.researchgate.net/profile/Nadia-Hussain-Al-Shammari/publication/338659778/figure/fig3/AS:848420117942272@1579290527960/Cyprinus-carpio-infected-with-bacterial-gill-disease-in-stations-2-and-3-with-A.jpg",
+    image:
+      "https://www.researchgate.net/profile/Nadia-Hussain-Al-Shammari/publication/338659778/figure/fig3/AS:848420117942272@1579290527960/Cyprinus-carpio-infected-with-bacterial-gill-disease-in-stations-2-and-3-with-A.jpg",
   },
   {
     name: "Argulus",
-    description: "Argulus, a common fish parasite, causes Argulosis by attaching to the fish, resulting in symptoms like appetite loss and excess mucus. Control includes natural remedies like papaya seed extracts and chemical treatments like dimilin.",
-    treatment: "Organophosphates,Masofen,Iufenuron,Diflubenzuron,Potassium permanganate,Emamectin benzoate",
-    image: "https://aquahealth.wordpress.com/wp-content/uploads/2013/11/argulus.png",
+    description:
+      "Argulus, a common fish parasite, causes Argulosis by attaching to the fish, resulting in symptoms like appetite loss and excess mucus. Control includes natural remedies like papaya seed extracts and chemical treatments like dimilin.",
+    treatment:
+      "Organophosphates,Masofen,Iufenuron,Diflubenzuron,Potassium permanganate,Emamectin benzoate",
+    image:
+      "https://aquahealth.wordpress.com/wp-content/uploads/2013/11/argulus.png",
   },
   {
     name: "Tail and Fin Rot",
-    description: "Fin and Tail Rot, caused by gram-negative bacteria, is a common and preventable aquarium fish disease often accompanying other illnesses. Difficult to cure, it can be fatal if untreated, spreading to all fish in the aquarium.",
+    description:
+      "Fin and Tail Rot, caused by gram-negative bacteria, is a common and preventable aquarium fish disease often accompanying other illnesses. Difficult to cure, it can be fatal if untreated, spreading to all fish in the aquarium.",
     treatment: "Tetracycline,Erythromychine,Chloromycetin,Acriflavine",
     image: "https://www.about-goldfish.com/images/Tail_Rot.jpg",
   },
   {
     name: "Epizootic Ulcerative Syndrome (EUS)",
-    description: "Epizootic Ulcerative Syndrome (EUS) is a fish disease caused by Aphanomyces invadans, marked by red spots evolving into ulcers due to fungal invasion of muscle tissue, leading to mycotic dermatitis granulomatosis.",
+    description:
+      "Epizootic Ulcerative Syndrome (EUS) is a fish disease caused by Aphanomyces invadans, marked by red spots evolving into ulcers due to fungal invasion of muscle tissue, leading to mycotic dermatitis granulomatosis.",
     treatment: "Cifax,Calcium Hydroxide,NaCl,CaO",
-    image: "https://assets.isu.pub/document-structure/210408181317-fa4ed68de2764c53140dbd30e8861947/v1/5f7e8e1790bbed506c6bf381561f612b.jpg",
+    image:
+      "https://assets.isu.pub/document-structure/210408181317-fa4ed68de2764c53140dbd30e8861947/v1/5f7e8e1790bbed506c6bf381561f612b.jpg",
   },
 ];
 
