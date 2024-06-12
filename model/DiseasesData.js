3c041a04f6c10978d67e9f29a44ad1f64f4367fe
herdifariz <fariz.herdi21@gmail.com>
2024-06-12 08:52:23 +0700

    update disease data

--- model/DiseasesData.js
+++ model/DiseasesData.js
@@ -1,20 +1,28 @@
 const diseases = [
   {
-    name: "White Tail",
+    name: "Argulus",
     description:
-      "White tail disease (WTD) or white muscle disease (WMD) is a viral infection caused by Macrobrachium rosenbergii nodavirus (MrNV) and its associated extra small virus (XSV).",
-    treatment: "Melafix,Pimafix,Formalin",
+      "Argulus, a common fish parasite, causes Argulosis by attaching to the fish, resulting in symptoms like appetite loss and excess mucus. Control includes natural remedies like papaya seed extracts and chemical treatments like dimilin.",
+    treatment:
+      "Organophosphates,Masofen,Iufenuron,Diflubenzuron,Potassium permanganate,Emamectin benzoate",
     image:
-      "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2021/12/06/2319888566.jpg",
+      "https://aquahealth.wordpress.com/wp-content/uploads/2013/11/argulus.png",
   },
   {
-    name: "Parasitic",
+    name: "Bacterial Aeromoniasis",
     description:
-      "Fish parasitic diseases result from infections, both internal and external, causing organ damage and system disruptions, crucially impacting aquaculture population health.",
-    treatment:
-      "Organophosphates,Formalin,Potassium Permanganate,2% Salt Bath,Malachite Green Oxalate",
+      "Bacterial aeromoniasis is a disease caused by the bacterium Aeromonas hydrophila in fish.",
+    treatment: "Pimafix,Furan-2,Triple Sulfa,E.M Erythromycin",
     image:
-      "http://www.wetwebmedia.com/Arthropoda/CrustaceanPIX/Isopod%20PIX/Anilocra%20laticaudataMD%20BONside.jpg",
+      "https://www.shutterstock.com/image-photo/fish-infectionskin-ulcerativeaeromonas-hydrophila-infect-600nw-1998990323.jpg",
+  },
+  {
+    name: "Bacterial Gill",
+    description:
+      "Bacterial gill disease (BGD) affects hatchery trout, causing gill filament clubbing. Environmental factors influence severity, impacting fish health by hindering respiration and balance. Similar issues can stem from diseases, toxins, or pollutants.",
+    treatment: "Melafix,Formalin,Praziquantel",
+    image:
+      "https://www.researchgate.net/profile/Nadia-Hussain-Al-Shammari/publication/338659778/figure/fig3/AS:848420117942272@1579290527960/Cyprinus-carpio-infected-with-bacterial-gill-disease-in-stations-2-and-3-with-A.jpg",
   },
   {
     name: "Bacterial Red/Red Spot",
@@ -25,12 +33,12 @@ const diseases = [
       "https://www.dpi.nsw.gov.au/__data/assets/image/0008/1268360/EUS-Bony-Bream-570x370.jpg",
   },
   {
-    name: "Bacterial Aeromoniasis",
+    name: "Epizootic Ulcerative Syndrome (EUS)",
     description:
-      "Bacterial aeromoniasis is a disease caused by the bacterium Aeromonas hydrophila in fish.",
-    treatment: "Pimafix,Furan-2,Triple Sulfa,E.M Erythromycin",
+      "Epizootic Ulcerative Syndrome (EUS) is a fish disease caused by Aphanomyces invadans, marked by red spots evolving into ulcers due to fungal invasion of muscle tissue, leading to mycotic dermatitis granulomatosis.",
+    treatment: "Cifax,Calcium Hydroxide,NaCl,CaO",
     image:
-      "https://www.shutterstock.com/image-photo/fish-infectionskin-ulcerativeaeromonas-hydrophila-infect-600nw-1998990323.jpg",
+      "https://assets.isu.pub/document-structure/210408181317-fa4ed68de2764c53140dbd30e8861947/v1/5f7e8e1790bbed506c6bf381561f612b.jpg",
   },
   {
     name: "Fungal Saprolegniasis",
@@ -42,21 +50,19 @@ const diseases = [
       "https://aquariumscience.org/wp-content/uploads/2020/08/fungus-9.jpg",
   },
   {
-    name: "Bacterial Gill",
-    description:
-      "Bacterial gill disease (BGD) affects hatchery trout, causing gill filament clubbing. Environmental factors influence severity, impacting fish health by hindering respiration and balance. Similar issues can stem from diseases, toxins, or pollutants.",
-    treatment: "Melafix,Formalin,Praziquantel",
-    image:
-      "https://www.researchgate.net/profile/Nadia-Hussain-Al-Shammari/publication/338659778/figure/fig3/AS:848420117942272@1579290527960/Cyprinus-carpio-infected-with-bacterial-gill-disease-in-stations-2-and-3-with-A.jpg",
+    name: "Healthy Fish",
+    description: "Your fish is healthy!",
+    treatment: "",
+    image: "",
   },
   {
-    name: "Argulus",
+    name: "Parasitic",
     description:
-      "Argulus, a common fish parasite, causes Argulosis by attaching to the fish, resulting in symptoms like appetite loss and excess mucus. Control includes natural remedies like papaya seed extracts and chemical treatments like dimilin.",
+      "Fish parasitic diseases result from infections, both internal and external, causing organ damage and system disruptions, crucially impacting aquaculture population health.",
     treatment:
-      "Organophosphates,Masofen,Iufenuron,Diflubenzuron,Potassium permanganate,Emamectin benzoate",
+      "Organophosphates,Formalin,Potassium Permanganate,2% Salt Bath,Malachite Green Oxalate",
     image:
-      "https://aquahealth.wordpress.com/wp-content/uploads/2013/11/argulus.png",
+      "http://www.wetwebmedia.com/Arthropoda/CrustaceanPIX/Isopod%20PIX/Anilocra%20laticaudataMD%20BONside.jpg",
   },
   {
     name: "Tail and Fin Rot",
@@ -66,12 +72,12 @@ const diseases = [
     image: "https://www.about-goldfish.com/images/Tail_Rot.jpg",
   },
   {
-    name: "Epizootic Ulcerative Syndrome (EUS)",
+    name: "White Tail",
     description:
-      "Epizootic Ulcerative Syndrome (EUS) is a fish disease caused by Aphanomyces invadans, marked by red spots evolving into ulcers due to fungal invasion of muscle tissue, leading to mycotic dermatitis granulomatosis.",
-    treatment: "Cifax,Calcium Hydroxide,NaCl,CaO",
+      "White tail disease (WTD) or white muscle disease (WMD) is a viral infection caused by Macrobrachium rosenbergii nodavirus (MrNV) and its associated extra small virus (XSV).",
+    treatment: "Melafix,Pimafix,Formalin",
     image:
-      "https://assets.isu.pub/document-structure/210408181317-fa4ed68de2764c53140dbd30e8861947/v1/5f7e8e1790bbed506c6bf381561f612b.jpg",
+      "https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2021/12/06/2319888566.jpg",
   },
 ];
 
